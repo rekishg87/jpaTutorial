b6e4ba083540df4bead87693ff5e8208c6a6bc5d
Rekish <rekishg87@gmail.com>
2015-09-11 15:32:12 +0200

    Completed example 3.2

--- src/main/java/com/javacodegeeks/ultimate/Main.java
+++ src/main/java/com/javacodegeeks/ultimate/Main.java
@@ -2,6 +2,7 @@ package com.javacodegeeks.ultimate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -33,6 +34,23 @@ public class Main {
             }
             if(factory != null) {
                 factory.close();
+            }
+        }
+    }
+
+    private void persistPerson(EntityManager entityManager) {
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+            Person person = new Person();
+            person.setFirstName("Homer");
+            person.setLastName("Simpson");
+            entityManager.persist(person);
+            transaction.commit();
+        } catch (Exception e) {
+            if(transaction.isActive()) {
+                transaction.rollback();
             }
         }
     }
